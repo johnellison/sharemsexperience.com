8e8e20e4d7e8e7fb4b8195d04e53056e1765888d
John Ellison <john@john-ellison.com>
2016-11-23 14:45:03 +0000

    update flow and add GA Events

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation();
+
+// GA Event Step 1
+$('#step1').click(function() {
+  console.log($(this).text());
+  ga('send', {
+    hitType: 'event',
+    eventCategory: 'apply',
+    eventAction: $(this).attr('id'),
+    eventLabel: $(this).text()
+  });
+});
+
+// GA Event Step 2
+$('#step2').click(function() {
+  console.log($(this).text());
+  ga('send', {
+    hitType: 'event',
+    eventCategory: 'apply',
+    eventAction: $(this).attr('id'),
+    eventLabel: $(this).text()
+  });
+});
